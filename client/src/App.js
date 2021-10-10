d6dd9949c6083c8bf04a72512b549cdc4b49718d
Brock Atwood <brock.atwood@du.edu>
2021-10-09 19:04:52 -0600

    added comments from video to resolver.js, created store.js in utils, and fixed app.js to run redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
+
+//comment out {StoreProvider} below
+// import { StoreProvider } from "./utils/GlobalState";
+
+//imports to run redux
+//Provider will serve as the component bridge between redux and the app. This will wrap or application component with the component from our react-redux library
+import { Provider } from "react-redux";
+//the value of this property is the whole store contained
+import store from "./utils/store";
+
+import Success from "./pages/Success";
+import OrderHistory from "./pages/OrderHistory";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
